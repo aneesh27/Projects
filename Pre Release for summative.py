59989634fa67f8d5b3bcab95903a785d63eba1ea
aneesh27 <45302898+aneesh27@users.noreply.github.com>
2018-11-24 09:06:16 +0530

    Update Pre Release for summative.py

--- Pre Release for summative.py
+++ Pre Release for summative.py
@@ -9,7 +9,7 @@ pay3=0
 pay4=0
 pay5=0
 
-for i in range (1,4):
+for i in range (1,31):
     emp_name = input("Enter Your Name: ")
     emp_num = int(input("Enter Number: "))
     hrs = int(input("Enter Number of hours worked: "))
